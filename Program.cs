c7c03eaa67c05439f802a8832f888f3bef109d0a
Viney <vineykhaneja999@gmail.com>
2022-03-11 08:27:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gambler_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Gambler Program");
+        }
+    }
+}
